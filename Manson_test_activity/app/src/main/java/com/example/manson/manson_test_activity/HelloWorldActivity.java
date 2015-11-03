cadd297314b49f6c77c84627ba8cd9279411a534
mansonchor.github.com <mansonchor.github.com>
2015-11-03 16:40:41 +0800

    first commit
    学习安卓之路现在开始

---
+++ Manson_test_activity/app/src/main/java/com/example/manson/manson_test_activity/HelloWorldActivity.java
@@ -0,0 +1,16 @@
+package com.example.manson.manson_test_activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2015/11/3.
+ */
+public class HelloWorldActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle saveIns){
+        super.onCreate(saveIns);
+        Log.d("test","ssss");
+    }
+}
