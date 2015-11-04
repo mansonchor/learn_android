3da2b13a125d504b1f66953f3e13f8784aec1d4b
mansonchor@126.com <mansonchor@126.com>
2015-11-04 22:03:43 +0800

    研究 dp和px的关系

--- Manson_test_activity/app/src/main/java/com/example/manson/manson_test_activity/HelloWorldActivity.java
+++ Manson_test_activity/app/src/main/java/com/example/manson/manson_test_activity/HelloWorldActivity.java
@@ -3,6 +3,10 @@ package com.example.manson.manson_test_activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2015/11/3.
@@ -12,5 +16,23 @@ public class HelloWorldActivity extends Activity {
     protected void onCreate(Bundle saveIns){
         super.onCreate(saveIns);
         setContentView(R.layout.hello_world_layout);
+
+        bindEvent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    private void bindEvent(){
+        Button button1Obj = (Button) findViewById(R.id.button_1);
+        button1Obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HelloWorldActivity.this,"you click button 1",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
